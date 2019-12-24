b49672b71bf46994c23c633cb88e74750f7573d6
Franco <francorulos08@gmail.com>
2019-12-24 19:30:35 -0300

    Deploying FrontEnd

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
 
     onSubmitSignIn = () => {
         try {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://young-wave-59475.herokuapp.com/signin', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
